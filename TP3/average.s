f9eca912589c906e011f1e8530b51a17a419870b
NanorJ <39227727+NanorJ@users.noreply.github.com>
2018-11-11 13:45:25 -0500

    average.s almost done

--- TP3/average.s
+++ TP3/average.s
@@ -4,8 +4,58 @@ matrix_row_aver_asm:
         push %ebp      			/* Save old base pointer */
         mov %esp, %ebp 			/* Set ebp to current esp */
 
-		/* Write your solution here */
+		#saving live registers
+        push %esi 
+        push %edi
+        push %ebx
+
+        #making room for 2 integers(r, c)
+        add $8, %esp
+        #putting 0 into r 
+        movl $0, -16(%ebp) 
+        #moving matorder into ebx
+        movl 16(%ebp), %ebx  
+L1:
+        #comparing value of r and matorder
+        cmp %ebx, -16(%ebp) 
+        #if matorder >= r jump to the break
+        jge BREAK1 
+
+        #assigning elem to edi and putting it to 0
+        mov $0, %edi
+
+        #putting 0 into c
+        movl $0, -20(%ebp) 
+L2:
+        #comparing value of c and matorder
+        cmp %ebx, -20(%ebp)
+        #if matorder >= c jump to the break
+        jge BREAK2 
+
+        #if the condition is respected continue
+        #moving the value of r into eax 
+        mov -16(%ebp), %eax
+        #multiply eax with ebx(contains matorder)
+        mul %ebx 
+        #add c to eax (r*matorder)
+        addl -20(%ebp), %eax 
+
+        #using ecx to store adress of inmatdata
+        movl 8(%ebp), %ecx 
+        #inmatdata[c + r * matorder]into ecx
+        movl (%ecx,%eax,4), %ecx 
+
+        #elem is edi and adding eax (elem+=)
+        addl %ecx, %edi 
+
+        #incrementer c
+        incl -20(%ebp)
+        jmp L2
+
+BREAK2:
+
+BREAK1: 
+
 		
         leave          			/* Restore ebp and esp */
         ret           			/* Return to the caller */
-		
